f8728ab948c40b5f0e776a575c9e337f4473fbc1
AnabFaiaz21 <116467647+AnabFaiaz21@users.noreply.github.com>
2023-11-06 19:07:18 +0530

    Commit

--- Streamlit_application_copy.py
+++ Streamlit_application_copy.py
@@ -1,11 +1,10 @@
-from google.cloud import texttospeech
 import streamlit as st
 import PyPDF2
-import os
-import json 
 from gtts import gTTS
-#google_application_credentials = st.secrets["GOOGLE_APPLICATION_CREDENTIALS"]
-#os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = google_application_credentials
+from gender_guesser.detector import Detector
+from googletrans import Translator
+
+
 
 def pdf_to_text(pdf_path):
     pdf_reader = PyPDF2.PdfReader(pdf_path)
@@ -16,24 +15,50 @@ def pdf_to_text(pdf_path):
         page = pdf_reader.pages[page_index]
         page_text = page.extract_text()
         extracted_text += page_text
- 
+
     return extracted_text.strip()
 
-languages = ['en', 'de', 'fr', 'es']
-def text_to_audio(text, languages=['en'], save_paths=None):
+
+from translate import Translator
+
+def translate_text(text, source_language='en', target_language='en'):
+    translator = Translator(from_lang=source_language, to_lang=target_language)
+    try:
+        translated_text = translator.translate(text)
+        return translated_text
+    except Exception as e:
+        print(f"Translation error: {e}")
+        return text
+
+
+
+def text_to_audio(text, text_language='en', voice_language='en', voice='neutral', save_path='output.mp3'):
     if not text:
         print("No text to convert to audio.")
         return
 
-    if save_paths is None:
-        save_paths = ['output_{}.mp3'.format(lang) for lang in languages]
+    # Translate text if the source and target languages are different
+    if text_language != voice_language:
+        text = translate_text(text, source_language=text_language, target_language=voice_language)
 
-    for lang, save_path in zip(languages, save_paths):
-        tts = gTTS(text=text, lang=lang, slow=False)
-        tts.save(save_path)
-        st.audio(save_path, format='audio/mp3')
-        print(f'Audiobook saved as {save_path}')
-st.title("Audiobook Generator")
+    if voice == 'neutral':
+        selected_voice_language = voice_language
+    else:
+        detector = Detector()
+        detected_gender = detector.get_gender(text.split()[0])
+
+        if detected_gender in ['male', 'female']:
+            selected_voice_language = f'{detected_gender}_{voice_language}'
+        else:
+            selected_voice_language = voice_language
+
+    tts = gTTS(text=text, lang=selected_voice_language, slow=False)
+    tts.save(save_path)
+    st.audio(save_path, format='audio/mp3')
+    print(f'Audiobook saved as {save_path}')
+
+
+st.title("Multilingual Audiobook Generator")
 
 uploaded_file = st.file_uploader("Upload a PDF file", type="pdf")
 
@@ -43,11 +68,15 @@ if uploaded_file is not None:
     # Extract text from the PDF
     extracted_text = pdf_to_text(uploaded_file)
 
-    # Language selection
-    language = st.selectbox("Select Language", ['en', 'de', 'fr', 'es'])
+    # Text language selection
+    text_language = st.selectbox("Select Text Language", ['en', 'de', 'fr', 'es'])
 
-    # Gender selection
-    gender = st.selectbox("Select Gender", ['neutral', 'male', 'female'])
+    # Voice (gender) language selection
+    voice_language = st.selectbox("Select Voice Language", ['en', 'de', 'fr', 'es'])
+
+    # Voice (gender) selection
+    voice_options = ['neutral', 'male', 'female']
+    voice = st.selectbox("Select Voice", voice_options)
 
     # Convert text to audio and display
-    text_to_audio(extracted_text, languages=[language], save_paths=None)
+    text_to_audio(extracted_text, text_language=text_language, voice_language=voice_language, voice=voice)
